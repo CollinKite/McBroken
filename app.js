f23cfc2668667d3d385c6962b56b6e6447c0bb10
AlexanderKugle <alexkugle@gmail.com>
2022-08-03 16:31:07 -0600

    Linked pages

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { response } = require('express');
 
 const app = express();
 const port = 3000;
@@ -14,12 +15,12 @@ app.set("view engine", "pug");
 
 app.get("/", (req,res) =>
 {
-    
+    res.render("home")
 })
 
 app.get("/location", (req,res) =>
 {
-    
+    res.render("location")
 })
 
 app.post("/location", (req,res) =>
@@ -29,10 +30,15 @@ app.post("/location", (req,res) =>
 
 app.get("/order", (req,res) =>
 {
-    
+    res.render("order")
 })
 
 app.post("/order", (req,res) =>
 {
     
+})
+
+app.listen(port, () =>
+{
+    console.log(`Listening on port ${port}`)
 })
